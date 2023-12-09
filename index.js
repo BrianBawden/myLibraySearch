d4cfbfcf36aa2cf2cb737397912645db3408ee90
Brian Bawden <brianbawden001@gmail.com>
2023-12-09 12:56:50 -0500

    get data for result from API

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const categorySpan = document.querySelectorAll(".category span")
 const userSearch = document.querySelector("userSearch")
 
 const hatchetUrl = "https://openlibrary.org/search.json?title=hatchet&author=gary%20Paulsen";
-const hatchetCoverUrl = "https://covers.openlibrary.org/b/isbn/9780140343717-L.jpg" // Hatchet cover
+const hatchetCoverUrl = "https://covers.openlibrary.org/b/isbn/9780140343717-S.jpg" // Hatchet cover
 const backupImage = "https://unsplash.com/photos/assorted-title-of-books-piled-in-the-shelves-NIJuEQw0RKg"
 
 async function dataRequest(url){
@@ -17,21 +17,24 @@ async function dataRequest(url){
   }
 }
 
+
 function urlRequest(url){
   dataRequest(url).then(data => {
     data.docs.forEach(book => {
+      console.log(book)
+      const coverUrl = 'https://covers.openlibrary.org/b/isbn/' + book.isbn[0] +'-L.jpg'
       results.innerHTML += `
         <div class="result">
         <div class="coverImg">
-          <img class="cover" src="./images/hatchet.jpg" alt="Cover of Hatchet">
+          <p>Cover:</p>
+          <img class="cover" src="${coverUrl}" alt="Cover of Hatchet">
         </div>
         <div class="info">
           <div>
-            <p class="resultTitle infoTxt">Title: Hatchet</p>
-            <p class="author infoTxt">Author: Gary Paulsen</p>
-            <p class="published infoTxt">Published Year: 1896</p>
-            <p class="pages infoTxt">Number of Pages: 192</p>
-            <p class="firstSentence infoTxt">Opening Sentence:<br> "Brian Robeson stared out the window of a small plane at the endless green northern wilderness below."</p>
+            <p class="resultTitle infoTxt">Title: "${book.title}"</p>
+            <p class="author infoTxt">Author: ${book.author_name}</p>
+            <p class="published infoTxt">Published Year: ${book.first_publish_year}</p>
+            <p class="pages infoTxt">Number of Pages: ${book.number_of_pages_median}</p>
           </div>
         </div>
       </div>
@@ -51,37 +54,3 @@ category.addEventListener("click", event => {
     event.target.classList.add("active")
   }
 })
-
-/*
-let data = dataRequest(hatchetUrl)
-
-
-data.then((value) => {
-  value.docs.forEach(element => {
-    console.log(element)
-    
-  });
-  // console.log(value.docs)
-})
-
-
-*/
-
-/*
-
-<div class="result">
-  <div class="coverImg">
-    <img class="cover" src="./images/hatchet.jpg" alt="Cover of Hatchet">
-  </div>
-  <div class="info">
-    <div>
-      <p class="resultTitle infoTxt">Title: Hatchet</p>
-      <p class="author infoTxt">Author: Gary Paulsen</p>
-      <p class="published infoTxt">Published Year: 1896</p>
-      <p class="pages infoTxt">Number of Pages: 192</p>
-      <p class="firstSentence infoTxt">Opening Sentence:<br> "Brian Robeson stared out the window of a small plane at the endless green northern wilderness below."</p>
-    </div>
-  </div>
-</div>
-
-*/
