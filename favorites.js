5f3e25c1f5e1ddef5a7176df6cb0e164d9262635
Brian Bawden <brianbawden001@gmail.com>
2023-12-13 20:44:03 -0500

    cleaned up code.

--- favorites.js
+++ favorites.js
@@ -1,56 +1,43 @@
-const results = document.querySelector(".results")
-const rmvBtn = document.querySelector
-// const favBookString = localStorage.getItem("favBooks")
-// let favBooks = {}
+const results = document.querySelector(".results");
 
-// if (favBookString){
-//   favBooks = JSON.parse(favBookString)
-// }
-// favBooks.forEach(book => {
-//   console.log(book)
-// })
-// }
+// build the cards for each book in the favBooks localStorage.
+function buildFavBooks() {
+  const favBookString = localStorage.getItem("favBooks");
+  let favBooks = {};
 
+  if (favBookString) {
+    favBooks = JSON.parse(favBookString);
 
-function buildFavBooks(){
-  const favBookString = localStorage.getItem("favBooks")
-  let favBooks = {}
-
-  if (favBookString){
-    favBooks = JSON.parse(favBookString)
-  }
-  favBooks.forEach(book => {
-    console.log("first: ", book)
-    results.innerHTML += `
-      <div class="result">
-      <div class="coverImg">
-        <a href="${book.bookCover}"><img class="cover" src="${book.bookCover}" alt="Cover of ${book.bookTitle}"></a>
-      </div>
-      <div class="info">
-        <div>
-          <p class="resultTitle infoTxt">"${book.bookTitle}"</p>
-          <p class="author infoTxt">Author: ${book.bookAuthor_name}</p>
-          <p class="published infoTxt">Published Year: ${book.bookFirst_publish_year}</p>
-          <p class="pages infoTxt">Number of Pages: ${book.bookNumber_of_pages_median}</p>
-          <button id="addFav" onclick='removeFromFavs("${book.bookTitle}")'>remove from Favorites</button>
+    favBooks.forEach((book) => {
+      const bookLink = "https://openlibrary.org" + book.bookKey;
+      results.innerHTML += `
+        <div class="result">
+        <div class="coverImg">
+        <a class="coverLink" href="${bookLink}" target="blank"><img class="cover" src="${book.bookCover}" alt="Cover of ${book.bookTitle}"></a>
+        </div>
+        <div class="info">
+          <div>
+          <a class="coverLink" href="${bookLink}" target="blank"><p class="resultTitle infoTxt">"${book.bookTitle}"</p></a>
+            <p class="author infoTxt">Author: ${book.bookAuthor_name}</p>
+            <p class="published infoTxt">Published Year: ${book.bookFirst_publish_year}</p>
+            <p class="pages infoTxt">Number of Pages: ${book.bookNumber_of_pages_median}</p>
+            <button id="addFav" onclick='removeFromFavs("${book.bookTitle}")'>remove from Favorites</button>
+          </div>
         </div>
       </div>
-    </div>
-    `
-  });
-  // console.log(data.docs)
+      `;
+    });
+  }
 }
 
-function removeFromFavs(delTitle){
-  let books = JSON.parse(localStorage.getItem('favBooks')) || []
+function removeFromFavs(delTitle) {
+  let books = JSON.parse(localStorage.getItem("favBooks"));
 
-  books = books.filter(book => book.bookTitle != delTitle)
+  books = books.filter((book) => book.bookTitle != delTitle);
 
-  localStorage.setItem('favBooks', JSON.stringify(books))
-  results.innerHTML = ""
-  buildFavBooks()
+  localStorage.setItem("favBooks", JSON.stringify(books));
+  results.innerHTML = "";
+  buildFavBooks();
 }
 
-
-
-buildFavBooks()
+buildFavBooks();
--- index.js
+++ index.js
@@ -1,46 +1,37 @@
-const results = document.querySelector(".results")
-const category = document.querySelector(".category")
-const categorySpan = document.querySelectorAll(".category span")
-const userSearch = document.querySelector("#userSearch")
-const search = document.querySelector("#search")
+const results = document.querySelector(".results");
+const category = document.querySelector(".category");
+const categorySpan = document.querySelectorAll(".category span");
+const userSearch = document.querySelector("#userSearch");
+const search = document.querySelector("#search");
 
-const hatchetUrl = "https://openlibrary.org/search.json?title=hatchet" //&author=gary%20Paulsen";
-const hatchetCoverUrl = "https://covers.openlibrary.org/b/isbn/9780140343717-S.jpg" // Hatchet cover
-const backupImage = "https://unsplash.com/photos/assorted-title-of-books-piled-in-the-shelves-NIJuEQw0RKg"
-const subjectOutdoors = 'https://openlibrary.org/subjects/outdoors.json?details=false'
+const backupImage =
+  "https://unsplash.com/photos/assorted-title-of-books-piled-in-the-shelves-NIJuEQw0RKg";
 
-async function dataRequest(url){
-  try{
-    const response = await fetch(url)
+// go to openLibrary API to get the requested json file.
+async function dataRequest(url) {
+  try {
+    const response = await fetch(url);
     const data = response.json();
-    // console.log(await data)
     return await data;
-  }catch(e){
-    console.log("dataRequest failed: ", e)
+  } catch (e) {
+    console.log("dataRequest failed: ", e);
   }
 }
 
-// this function gets the number used by the API to find images of the cover. 
-function getIsbn(isbns){
-  if(isbns){
-    return 'https:\//covers.openlibrary.org/b/isbn/' + isbns[0] +'-L.jpg'
-  }
-  else{
-    return 'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D';
+// this function gets the number used by the API to find images of the cover.
+function getIsbn(isbns) {
+  if (isbns) {
+    return "https://covers.openlibrary.org/b/isbn/" + isbns[0] + "-L.jpg";
+  } else {
+    return backupImage;
   }
 }
 
-function clearSearch() {
-  results.innerHTML = ""
-}
-
-
-function urlRequest(url){
-  dataRequest(url).then(data => {
-    data.docs.forEach(book => {
-      // console.log(book.key)
-      const bookLink = "https://openlibrary.org" + book.key
-      let coverImg = getIsbn(book.isbn)
+function urlRequest(url) {
+  dataRequest(url).then((data) => {
+    data.docs.forEach((book) => {
+      const bookLink = "https://openlibrary.org" + book.key;
+      const coverImg = getIsbn(book.isbn);
       results.innerHTML += `
         <div class="result">
         <div class="coverImg">
@@ -52,58 +43,54 @@ function urlRequest(url){
             <p class="author infoTxt">Author: ${book.author_name}</p>
             <p class="published infoTxt">Published Year: ${book.first_publish_year}</p>
             <p class="pages infoTxt">Number of Pages: ${book.number_of_pages_median}</p>
-            <button id="addFav" onclick='addToFavs("${book.title}", "${book.author_name}", "${book.first_publish_year}", "${book.number_of_pages_median}", "${coverImg}", "${book.key})'>Add to Favorites</button>
+            <button id="addFav" onclick='addToFavs("${book.title}", "${book.author_name}", "${book.first_publish_year}", "${book.number_of_pages_median}", "${coverImg}", "${book.key}")'>Add to Favorites</button>
           </div>
         </div>
       </div>
-      `
+      `;
     });
-    console.log(data.docs)
-  })
+  });
 }
 
-// urlRequest(hatchetUrl)
-// dataRequest('https://openlibrary.org/subjects/outdoors.json?details=false')
+function addToFavs(title, author, published, pages, cover, key) {
+  console.log(key);
+  book = {
+    bookTitle: title,
+    bookAuthor: author,
+    bookPublished: published,
+    bookPages: pages,
+    bookCover: cover,
+    bookKey: key,
+  };
 
-function addToFavs(title, author, published, pages, cover, key){
-console.log(key)
-  book = {bookTitle: title, bookAuthor: author, bookPublished: published, bookPages: pages, bookCover: cover, bookKey: key}
-  
-  const favBookLS = localStorage.getItem('favBooks')
+  const favBookLS = localStorage.getItem("favBooks");
 
-  if (favBookLS){
-    const oldFavBooks = JSON.parse(favBookLS)
-    const newFavBooks = [...oldFavBooks, book]
+  if (favBookLS) {
+    const oldFavBooks = JSON.parse(favBookLS);
+    const newFavBooks = [...oldFavBooks, book];
 
-  localStorage.setItem('favBooks', JSON.stringify(newFavBooks))
-  }else{
-    localStorage.setItem('favBooks', JSON.stringify([book]))
-
+    localStorage.setItem("favBooks", JSON.stringify(newFavBooks));
+  } else {
+    localStorage.setItem("favBooks", JSON.stringify([book]));
   }
 }
 
-
-// listen to see which search parameter is being clicked. Category is the div area the search type buttons are located. 
-category.addEventListener("click", event => {
-  if(event.target.tagName === "SPAN"){
-    // results.innerHTML =  ""
-    // urlRequest(event.target.dataset.url)
-    categorySpan.forEach(item => item.classList.remove("active"))
-    event.target.classList.add("active")
+// listen to see which search parameter is being clicked. Category is the div area the search type buttons are located.
+category.addEventListener("click", (event) => {
+  if (event.target.tagName === "SPAN") {
+    categorySpan.forEach((item) => item.classList.remove("active"));
+    event.target.classList.add("active");
   }
-})
+});
 
-// when the search button is clicked the active button is found and used in conjunction with the text from the search to build a url for the search.
-search.addEventListener('click', event => {
-  const search = userSearch.value
-  // location.reload()
-  // console.log(userSearch.value)
-  const replaceSearch = search.trim().replace(/\s/g, "+")
-  const activeSearch = document.querySelector(".active").dataset.url
-  const url = activeSearch + replaceSearch
-  clearSearch()
-  urlRequest(url)
+// when the search button is clicked the active class is found and used in conjunction with the text from the search to build a url for the search.
+search.addEventListener("click", (event) => {
+  const search = userSearch.value;
+  const replaceSearch = search.trim().replace(/\s/g, "+");
+  const activeSearch = document.querySelector(".active").dataset.url;
+  const url = activeSearch + replaceSearch;
+  results.innerHTML = "";
+  urlRequest(url);
 
-  userSearch.value = ""
-  
-})
+  userSearch.value = "";
+});
